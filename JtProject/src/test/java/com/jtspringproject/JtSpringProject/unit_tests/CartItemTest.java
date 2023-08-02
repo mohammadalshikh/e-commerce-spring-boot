49c4f71d1024082ae4b6aef7bfaa3108d88563cd
Emma <3lddemmanounke@gmail.com>
2023-08-02 17:59:49 -0400

    Added tests for CustomCartItem.java

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/unit_tests/CartItemTest.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/unit_tests/CartItemTest.java
@@ -1,3 +1,4 @@
+package com.jtspringproject.JtSpringProject;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
---
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/unit_tests/CustomCartItemTest.java
@@ -0,0 +1,36 @@
+package com.jtspringproject.JtSpringProject;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CustomCartItemTest {
+
+    @Test
+    public void testCustomCartItem() {
+        String productName = "Product 1";
+        int quantity = 2;
+        float totalPrice = 50.0f;
+        int productID = 101;
+
+        CustomCartItem customCartItem = new CustomCartItem(productName, quantity, totalPrice, productID);
+
+        assertEquals(productName, customCartItem.getProductName());
+        assertEquals(quantity, customCartItem.getQuantity());
+        assertEquals(totalPrice, customCartItem.getTotalPrice(), 0.001); // Using delta to handle float comparison
+        assertEquals(productID, customCartItem.getProductID());
+    }
+
+    @Test
+    public void testGettersWithDifferentValues() {
+        String productName = "Product 2";
+        int quantity = 5;
+        float totalPrice = 20.0f;
+        int productID = 102;
+
+        CustomCartItem customCartItem = new CustomCartItem(productName, quantity, totalPrice, productID);
+
+        assertEquals(productName, customCartItem.getProductName());
+        assertEquals(quantity, customCartItem.getQuantity());
+        assertEquals(totalPrice, customCartItem.getTotalPrice(), 0.001); // Using delta to handle float comparison
+        assertEquals(productID, customCartItem.getProductID());
+    }
+}
